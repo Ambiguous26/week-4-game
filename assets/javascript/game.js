32fa19dc4a369730276da09b8d2c37df3ba0700e
Aram <avalbaro@gmail.com>
2017-09-22 16:21:00 -0700

    updates to my js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,76 @@
 
 
 	
-	var	randomNumber = {Math.floor((Math.random() * 120) + 1);
-	
+	var	game = {
+		randomNumber: Math.floor((Math.random() * (120-18)) + 19),
+		randomCrystalNumber: Math.floor((Math.random() * 12) + 1 ),
+		newValue: "randomCrystalNumber",
+		wins: 0,
+		losses: 0,
+
+
+		// a random number will be genereated between 19-120	
 	generateRandomNumber: function() {
-		getElementById("three").innerHTML(randomNumber);
-		document.getElementById("three").onclick = randomNumber;
+		// this makes it so a random number is generated every time the page is refresed
+		this.randomNumber = Math.floor((Math.random() * (120-18)) + 19);
+		// number will be pushed to the html
+		document.getElementById("three").innerHTML = this.randomNumber;
+		// how you can see that the function works properly
+		// console.log(this.randomNumber);
+
+		// how the random number generator function was tested
+			// for (var i = 0; i < 1000; i++){
+			// console.log(Math.floor((Math.random() * (120-18)) + 19));
+			// var newRandomnumber = Math.floor((Math.random() * (120-18)) + 19)
+			// if (newRandomnumber < 19 || newRandomnumber > 120)
+			// 	alert ("look out!");
+	
 		},
 
+	generateRandomCrystalNumber: function(){
+			// checked that number was generating properly -- received no alert
+		 		// for (var i = 0; i < 100; i++){
+				// 	console.log(Math.floor((Math.random() * 12) + 1 ));
+				// var newRandomCystalNumber = Math.floor((Math.random() * 12) + 1 );
+				// if (newRandomCystalNumber < 1 || newRandomCystalNumber> 12)
+				// 	alert("look out!");
+			document.getElementById("red").onclick = this.generateRandomCrystalNumber;
+			this.randomCystalNumber = Math.floor((Math.random() * 12) + 1 );
 
-		// a random number will be genereated between 19-120
-		// number will be pushed to the html
+			document.getElementById("red").value = this.randomCrystalNumber;
+			// tested that amount was being generated for red crystal
+				// console.log(document.getElementById("red").value = this.randomCrystalNumber);
+			document.getElementById("red").onclick = this.generateRandomCrystalNumber;
 
-	} //end of script
+		
+
+		},
+	
+
+	} //end of object
+
+
+
+
+
+
+
+
+
+
 	// Actions when player clicks on a cystal
 		// button on click event will add an amout to the counter
 		// each crystal should have a value between 1-12
 		// pushed effects to the html
 
-	// rules for winning/lossing
+// need to call this function outside of object for it to run
+game.generateRandomNumber();
+game.generateRandomCrystalNumber();
+// console.log(generateRandomCrystalNumber)
+
+
+
+// rules for winning/lossing
 		// conditional statement: 
 		// if the number === the amount generated game is won
 		// add 1 to total wins.
@@ -33,6 +85,11 @@
 
 	// games restarts automatically and a new number is generated
 	// score counter resets to 0
+
+
+
+
+
 
 
 
